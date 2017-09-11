8799a2d1171bc46d7907a6737668680841b43ec6
Fernando <fzavala649@gmail.com>
2017-09-10 23:25:03 -0700

    Update fork_exec.c
    Changed it so that the child sends a message to the parent. Also made the parent wait for the child before exiting.

--- mini-project/fork_exec.c
+++ mini-project/fork_exec.c
@@ -1,52 +1,72 @@
-#include < sys/types.h >
-#include < stdio.h >
-#include < string.h >
-#include < unistd.h >
-#define BUFFER SIZE 25
-#define READ END 0
-#define WRITE END 1
+/**
+ * Example program demonstrating UNIX pipes.
+ *
+ * Figures 3.25 & 3.26
+ *
+ * @author Silberschatz, Galvin, and Gagne
+ * Operating System Concepts  - Ninth Edition
+ * Copyright John Wiley & Sons - 2013
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define BUFFER_SIZE 25
+#define READ_END	0
+#define WRITE_END	1
 
 int main(void)
 {
-  char write msg[BUFFER SIZE] = "Greetings";
-  char read msg[BUFFER SIZE];
-  int fd[2];
-  pid t pid;
+	char write_msg[BUFFER_SIZE] = "Greetings";
+	char read_msg[BUFFER_SIZE];
+	pid_t pid;
+	int fd[2];
 
-  /* create the pipe */
-  if (pipe(fd) == -1) {
-    fprintf(stderr,"Pipe failed");
-    return 1;
-  }
-  
-  /* fork a child process */
-  pid = fork();
-  if (pid < 0) { /* error occurred */
-    fprintf(stderr, "Fork Failed");
-    return 1;
-   }
-  
-  if (pid > 0) { /* parent process */
-    /* close the unused end of the pipe */
-    close(fd[READ END]);
-   
-   /* write to the pipe */
-    write(fd[WRITE END], write msg, strlen(write msg)+1);
-   
-   /* close the write end of the pipe */
-    close(fd[WRITE END]);
-   }
-  else { /* child process */
-    /* close the unused end of the pipe */
-    close(fd[WRITE END]);
-    
-    /* read from the pipe */
-    read(fd[READ END], read msg, BUFFER SIZE);
-    printf("read %s",read msg);
-   
-   /* close the write end of the pipe */
-    close(fd[READ END]);
-  }
-  
-  return 0;
+	/* create the pipe */
+	if (pipe(fd) == -1) {
+		fprintf(stderr,"Pipe failed");
+		return 1;
+	}
+
+	/* now fork a child process */
+	pid = fork();
+
+	if (pid < 0) {
+		fprintf(stderr, "Fork failed");
+		return 1;
+	}
+
+/*STart of msg passing between child and parent */
+
+
+	if (pid == 0) {  /* child process */
+		/* close the unused end of the read pipe */
+		close(fd[READ_END]);
+
+		/* write to the write end of the pipe */
+		write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
+		printf("\nchild end of pipe -- sending: '%s'\n",write_msg);
+
+		/* close the write end of the pipe */
+		close(fd[WRITE_END]);
+	}
+	else { /* parent process*/
+		/* close the unused end of write the pipe */
+		close(fd[WRITE_END]);
+
+		/* read from the pipe */
+		read(fd[READ_END], read_msg, BUFFER_SIZE);
+		printf("\nparent end of pipe -- received: %s\n",read_msg);
+
+		/* close the write end of the pipe */
+		close(fd[READ_END]);
+		wait(NULL);
+		exit(EXIT_SUCCESS);
+	}
+
+	return 0;
 }
