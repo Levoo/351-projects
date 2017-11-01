e0024c1c7a3adeb806103d18f65b9e24eac69c31
Fernando <fzavala649@gmail.com>
2017-11-01 03:28:44 -0700

    see todo at top

--- projects/bankersAlgo/banksyy.cpp
+++ projects/bankersAlgo/banksyy.cpp
@@ -6,6 +6,14 @@ Project: Multi-threaded Bankers Algorithm
 Due:
 File name: banksy.cpp
 Status: WIP
+ todo: -request / release functions
+       -bankers algo
+       -array of bools to keep track of done processes
+       -handle accepted / denied requests?
+       -handel unsafe state -> should exit whole program and print current state and which process was requesting and what
+       it was requesting
+
+
 ---------------------------------------------*/
 
 #include <iostream>
@@ -41,6 +49,7 @@ std::mutex transaction_lock;
 // these fucntions will be accessed through the bank_customer function
 int request_resources(int customer_num, int request[]);
 int release_resources(int customer_num, int release[]);
+bool inSafeState(int customer_num, int request[]); //call this after a resource is released to see if we are in a safe state
 
 
 void bank_customer(int customer_num);
@@ -54,9 +63,9 @@ void bank_customer(int customer_num);
 // need table will be [ # of customer}[max - allocated]
 // sleep thread used to make it seem that the thread is doing work
 void create_Tables(char* given[]);
-void show_Table(); // for matric array
+void show_Table(int table[][NUM_RESOURCES]); // for matric array
 void show_Vector(int vect[]); // for 1d array
-void show_All();
+void show_State();
 void sleep_thread();
 
 
@@ -65,7 +74,9 @@ int main(int argc, char* argv[]){
     // create the tables based on avalable and other such things
     // possibly make it read a file for easy testing, lookm at java code for more
     // refined help, currently seems good but there might be at least 3 funcs missing.
+    transaction_lock.lock();
     create_Tables(argv);
+    transaction_lock.unlock();
 
     std::cout << "Customers Are entering the bank\n";
 
@@ -83,35 +94,75 @@ int main(int argc, char* argv[]){
     std::cout << "Customers are leaving\n";
     return 0;
 }
+//-----------------------------------------------------------------
 //create and populate all the tables for the bankers algorithm
 void create_Tables(char* given[]){
     for(int i = 0; i < NUM_RESOURCES; i++){
-        std::cout << given[i] << std::endl; // given[1..3] has the numbers given of 0 has text so start at i+1
+        //std::cout << given[i] << std::endl; // given[1..3] has the numbers given of 0 has text so start at i+1
         available[i] = atoi(given[i+1]); // set the avaliable to the one given by the user
     }
-    std::cout << available[0] << " " << available[1] << " " << available[2] << std::endl;
     std::fstream readFile;
-    readFile.open("/home/dauser/CLionProjects/bank_algo/infile.txt");
+    readFile.open("/home/osc-vm/CLionProjects/banker_algo/infile.txt"); //change for other sys
 
     if(!readFile) {std::cout << "error opening file!"; exit(1);}
     std::string temp;
+    int raw_data[30];
+    int i = 0;
     while (readFile >> temp){ //raw reading figure out how to parse the data
-        std::cout << temp << " ";
+        raw_data[i] = stoi(temp);
+        i++;
+    }
+    // allocation and max table
+    int allocLoc[5] = {0, 6, 12, 18, 24};  // too cheesy figure out how to do with loops
+    int maxLoc[5]   = {3, 4, 15, 21, 27};
+    for(int customer = 0; customer < NUM_CUSTOMERS; customer++){ //move raw data into arrays
+        for(int data = 0; data < 3; data++){
+            allocation[customer][data] = raw_data[allocLoc[customer] + data];
+            maximum[customer][data]    = raw_data[maxLoc[customer] + data];
+        }
+
+    }
+    // need table
+    for(int customer = 0; customer < NUM_CUSTOMERS; customer++){
+        for(int data = 0; data < 3; data++){
+            need[customer][data] = maximum[customer][data] - allocation[customer][data];
+        }
+    }
+    //get true avaliablity, have al the columns added up and subtract frm total resources provided by user
+    int r_temp[NUM_RESOURCES] = {0};
+    for(int customer = 0; customer < NUM_CUSTOMERS; customer++){
+        for(int data = 0; data < 3; data++){
+            r_temp[data] += allocation[customer][data];
+        }
+    }
+
+    for(int data = 0; data < 3; data++){
+        available[data] -= r_temp[data];
     }
 }
 
 
 //----------------------------------------------------------------------------------------
+// other functions used to check
 
 int request_resources(int customer_num, int request[]){
     // do something
-    std::cout << request << std::endl;
+    //std::cout << request << std::endl;
+    //1.) if request <= need go to step 2
+    //2.) if request <= avaliable go to 3
+    //3.) have sys prentent to give resources to process
+    // avliable = avaliable - request
+    // allocation  = allocation + request
+    // need = need - request
     std::cout << "Customer # " << customer_num << " made a resource request.\n";
     return 0;
+
 }
 
 int release_resources(int customer_num, int release[]){
     // do something
+    //std::lock_guard<std::mutex> guard(transaction_lock);
+    // release all of the allocated resources back to avaliable so avaliable += allocated
     std::cout << "Customer # " << customer_num << " made a release request.\n";
     return 0;
 }
@@ -121,13 +172,55 @@ void sleep_thread(){
     std::this_thread::sleep_for(sleepDuration);
 }
 
+void show_Table(int table[][NUM_RESOURCES]){
+    for(int customer = 0; customer < NUM_CUSTOMERS; customer++){
+        for(int data = 0; data < 3; data++){
+            std::cout << table[customer][data] << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+
 void show_Vector(int vect[]){
+    //std::lock_guard<std::mutex> guard(transaction_lock);
     for (int i = 0; i < NUM_RESOURCES; i++){
         std::cout << vect[i] << " ";
     }
     std::cout << std::endl;
 }
+
+void show_State(){
+    bool printed = false;
+    std::cout << "\t\t\tCurrent State\n";
+    std::cout << "Customer    | Allocated  | Max       | Need    | Available ";
+    for(int customer = 0; customer < NUM_CUSTOMERS; customer++){ //move raw data into arrays
+        std::cout << "\nC " << customer+1 << "\t\t    | ";
+        for(int data = 0; data < 3; data++){
+            std::cout << allocation[customer][data] <<  " ";
+        }
+        std::cout << "\t | ";
+        for(int data = 0; data < 3; data++){
+            std::cout << maximum[customer][data] <<  " ";
+        }
+
+        std::cout << "\t | ";
+        for(int data = 0; data < 3; data++){
+            std::cout << need[customer][data] <<  " ";
+        }
+
+        if (!printed){
+            std::cout << "  |> ";
+            for(int data = 0; data < 3; data++){
+                std::cout << available[data] <<  " ";
+            }
+            printed = true;
+        }
+
+    }
+    std::cout << std::endl;
+}
 //----------------------------------------------------------------------------------------
+// function threads use
 
 void bank_customer(int customer_num){
     // requestes and such will be made here
@@ -136,20 +229,27 @@ void bank_customer(int customer_num){
 
     while(running) {
         sleep_thread();
-        transaction_lock.lock();
+        //transaction_lock.lock();
         //make a resource request make it random use java file.
-        for(int req = 0; req < NUM_RESOURCES; req++){ customer_request[req] = maximum[customer_num][req]+1;}
+        transaction_lock.lock();
+        show_State(); //put an assert to make sure we get no errors in random generator
+        for(int req = 0; req < NUM_RESOURCES; req++){ customer_request[req] = rand() % (maximum[customer_num][req]+1);}
         show_Vector(customer_request);
+        transaction_lock.unlock();
+        //show_State();
         // if request was succesful exit then stop the loop
         // if not keep it as 1
-        running = request_resources(customer_num, nullptr);
+        transaction_lock.lock();
+        //show_State();
+        if(request_resources(customer_num, customer_request) == 0){
+            sleep_thread(); //make it look likes its using the resources
+            release_resources(customer_num, customer_request); //implement
+            running = 0;
+            // have array of bools and set this process to done (true)
+            // possiby have statment saying thread # n requested this and show new state after release?
+        }
         transaction_lock.unlock();
+       // release_resources(customer_num, nullptr);
     }
     // release resources back to avaliable
-    transaction_lock.lock();
-    release_resources(customer_num, nullptr);
-    transaction_lock.unlock();
-
-    //
 }
-
