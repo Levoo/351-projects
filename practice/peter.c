8bcefcff67c39fb2632d0194adf019fa59757675
Fernando <fzavala649@gmail.com>
2017-11-02 16:43:13 -0700

    Create peter.c

---
+++ practice/peter.c
@@ -0,0 +1,57 @@
+#include <pthread.h>
+#include <stdio.h>
+
+typedef enum defer{p0,p1}defer;
+defer should_defer = p0;
+int p0_wantsCritical = 0;
+int p1_wantsCritical = 0;
+
+void* P0(){
+  int finished = 0;
+  while(!finished){
+    p0_wantsCritical = 1;
+    should_defer = p1;
+    while(p1_wantsCritical && should_defer == p1){ /*do noting*/}
+    /*critical*/
+    p0_wantsCritical = 0;
+    printf("p0 is critical\n");
+    should_defer = p0;
+    p0_wantsCritical = 0;
+    }
+
+  return NULL;
+
+}
+
+void* P1(){
+  int finished = 0;
+  while(!finished){
+    p0_wantsCritical = 1;
+    should_defer = p1;
+    while(p1_wantsCritical && should_defer == p1){ /*do noting*/}
+    /*critical*/
+    p0_wantsCritical = 0;
+    printf("p1 is critical\n");
+    should_defer = p0;
+    p0_wantsCritical = 0;
+    }
+
+}
+
+
+int main(){
+    pthread_attr_t att;
+    pthread_t thread0;
+    pthread_t thread1;
+
+    pthread_attr_init(&att);
+
+    pthread_create(&thread0, &att, P0, NULL);
+    pthread_create(&thread1, &att, P1, NULL);
+
+    pthread_join(thread0, NULL);
+    pthread_join(thread1, NULL);
+
+
+  return 0;
+}
