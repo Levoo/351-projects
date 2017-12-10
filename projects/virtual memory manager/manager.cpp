f33f41db30cd295c0f8883873155c4874a84b869
Fernando <fzavala649@gmail.com>
2017-12-09 21:33:41 -0800

    Update manager.cpp

--- projects/virtual memory manager/manager.cpp
+++ projects/virtual memory manager/manager.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 
 int main(int argc, char ** argv) {
-    if(argc < 2){
+    if(argc != 2){
         std::cout << "Not enough enough argumetns\n\t Usage ./a.out file.txt\n";
         return 1;
     }
